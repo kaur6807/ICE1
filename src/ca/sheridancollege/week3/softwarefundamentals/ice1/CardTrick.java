fcbfe345b7910f95c89acf0c78b4495f6d57de6f
kaur6807 <50635243+kaur6807@users.noreply.github.com>
2019-05-22 02:07:01 -0400

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -21,11 +21,30 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
+          //c.setValue(insert call to random number generator here)
+            c.setValue(c.getRandom());
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[c.getRandomSuit()]);
+            magicHand[i]=c;
+            
         }
         
         //insert code to ask the user for Card value and suit, create their card
+        System.out.println("Suit you want to enter : ");
+        String suit=input.nextLine();
+        System.out.println("Enter any value from 1 to 13");
+        int number=input.nextInt();
+        for(int j=0; j<magicHand.length;j++){
+            if(suit.equals(magicHand[j].getSuit()) && number==magicHand[j].getValue()){
+                System.out.println("Found");
+            }
+            else{
+                System.out.println("Not Found!");
+                
+            }
+            
+            
+        }
         // and search magicHand here
         //Then report the result here
     }
